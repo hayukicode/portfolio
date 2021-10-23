da9ecf07ccf39cd95ebee44909e383c9060e1b9c
Wellington Henrique <70868758+hayukicode@users.noreply.github.com>
2021-10-22 23:28:26 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+ 
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "20px",
+  duration: 2000,
+  reset: true,
+});
+
+//Home delay
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".imagem-delay", { delay: 200 });
+sr.reveal(".card-delay", { delay:900 });
+sr.reveal(".scroll-delay", { delay: 1000 });
+sr.reveal(".titulo-delay", { delay: 200 });
+sr.reveal(".svg-interval", { delay: 1500 });
+
+
+
+
+const navbar = ScrollReveal({
+  origin: "top",
+  distance: "20px",
+  duration: 2000,
+  reset: true,
+});
+navbar.reveal(".nava", { delay: 200 });
